a3b3b050dae3f63a8a76193c3eb57235bf6d6f65
abdoSobhy1 <abdoosobhy97@gmail.com>
2022-10-09 19:54:58 +0200

    slides

--- scripts/main.js
+++ scripts/main.js
@@ -36,8 +36,6 @@ var day = hour * 24
 var month = day*30
 var year = month *12 + 5.25 * day
 
-console.log(year);
-
 let counter = setInterval(() => {
   // Get Date Now
   let dateNow = new Date().getTime();
@@ -67,3 +65,36 @@ document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : se
     clearInterval(counter);
   }
 }, 1000);
+
+let progressSpans = document.querySelectorAll(".the-progress span");
+let section = document.querySelector(".our-skills");
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+// Skills Animate Width
+if (window.scrollY >= section.offsetTop - 500) {
+    progressSpans.forEach((span) => {
+    span.style.width = span.dataset.progress;
+    });
+}
+// Stats Increase Number
+// if (window.scrollY >= statsSection.offsetTop - 800) {
+//     if (!started) {
+//     nums.forEach((num) => startCount(num));
+//     }
+//     started = true;
+// }
+};
+
+function startCount(el) {
+let goal = el.dataset.goal;
+let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+    clearInterval(count);
+    }
+}, 2000 / goal);
+}
